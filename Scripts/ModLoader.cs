adfbc9586daf5ff3aeec5a2a3885df56669cdef3
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-06 16:48:00 -0500

    Add desc / game version properties

--- Scripts/ModLoader.cs
+++ Scripts/ModLoader.cs
@@ -196,7 +196,9 @@ namespace ModLoader
     {
         public string Name { get; set; }
         public string Author { get; set; }
+        public string Description { get; set; }
         public string Version { get; set; }
+        public string[] GameVersions { get; set; }
         public string[] Dependencies { get; set; }
     }
 }
