26c7fbdc86467c63634efcf4ad87567db9cb396e
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-10 21:29:39 -0500

    more utils

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -9,5 +9,12 @@ namespace Valk.Modules
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
+
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float by)
+        {
+            float retX = Mathf.Lerp(a.x, b.x, by);
+            float retY = Mathf.Lerp(a.y, b.y, by);
+            return new Vector2(retX, retY);
+        }
     }
 }
