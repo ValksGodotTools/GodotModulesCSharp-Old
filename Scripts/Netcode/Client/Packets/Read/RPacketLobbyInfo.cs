4f8f2f62c6ce5a0fccf74a39e07f5f9b400bd6bd
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-18 20:10:11 -0500

    RPacketLobbyInfo

---
+++ Scripts/Netcode/Client/Packets/Read/RPacketLobbyInfo.cs
@@ -0,0 +1,25 @@
+using Common.Netcode;
+using System.Collections.Generic;
+
+namespace GodotModules.Netcode.Client 
+{
+    public class RPacketLobbyInfo 
+    {
+        public uint Id { get; set; }
+        public Dictionary<uint, string> Players { get; set; }
+
+        public RPacketLobbyInfo(PacketReader reader) 
+        {
+            Id = reader.ReadUInt16();
+            var count = reader.ReadUInt16();
+            Players = new Dictionary<uint, string>();
+            for (int i = 0; i < count; i++)
+            {
+                var id = reader.ReadUInt16();
+                var name = reader.ReadString();
+
+                Players.Add(id, name);
+            }
+        }
+    }
+}
