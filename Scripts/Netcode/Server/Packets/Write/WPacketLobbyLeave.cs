10eb5e12db74df07cb4b9d53b900dffaf0cd206f
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-18 20:12:13 -0500

    WPacketLobbyLeave

---
+++ Scripts/Netcode/Server/Packets/Write/WPacketLobbyLeave.cs
@@ -0,0 +1,14 @@
+using Common.Netcode;
+
+namespace GodotModules.Netcode.Server
+{
+    public class WPacketLobbyLeave : IWritable 
+    {
+        public uint Id { get; set; }
+
+        public void Write(PacketWriter writer)
+        {
+            writer.Write((ushort)Id);
+        }
+    }
+}
