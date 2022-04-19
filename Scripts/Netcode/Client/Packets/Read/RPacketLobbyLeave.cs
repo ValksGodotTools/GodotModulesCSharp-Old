08e12c51ea53767ca4aeaa3bb4a9de4c3dfedf23
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-18 20:10:17 -0500

    RPacketLobbyLeave

---
+++ Scripts/Netcode/Client/Packets/Read/RPacketLobbyLeave.cs
@@ -0,0 +1,14 @@
+using Common.Netcode;
+
+namespace GodotModules.Netcode.Client 
+{
+    public class RPacketLobbyLeave
+    {
+        public uint Id { get; set; }
+
+        public RPacketLobbyLeave(PacketReader reader)
+        {
+            Id = reader.ReadUInt16();
+        }
+    }
+}
