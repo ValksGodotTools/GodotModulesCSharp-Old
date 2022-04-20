f734cf3795cd53b26453d565435b1241fd4ba9c9
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-20 12:55:03 -0500

    Refactor utils

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -1,8 +1,5 @@
 using Godot;
-using System.Collections.Generic;
 using System;
-using System.Linq;
-using System.Reflection;
 
 namespace GodotModules
 {
@@ -15,25 +12,6 @@ namespace GodotModules
             Console.ResetColor();
         }
 
-        public static string StringifyDict<Key, Value>(Dictionary<Key, Value> dict) => string.Join(" ", dict.Select(x => $"{x.Key} {x.Value}"));
-
-        public static float Remap(this float value, float from1, float to1, float from2, float to2)
-        {
-            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
-        }
-
-        public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
-        {
-            float retX = Mathf.Lerp(a.x, b.x, t);
-            float retY = Mathf.Lerp(a.y, b.y, t);
-            return new Vector2(retX, retY);
-        }
-
-        public static Dictionary<Key, Value> LoadInstances<Key, Value, Namespace>() =>
-            Assembly.GetExecutingAssembly()
-                .GetTypes()
-                .Where(x => typeof(Value).IsAssignableFrom(x) && !x.IsAbstract && x.Namespace == typeof(Namespace).Namespace)
-                .Select(Activator.CreateInstance).Cast<Value>()
-                .ToDictionary(x => (Key)Enum.Parse(typeof(Key), x.GetType().Name.Replace(typeof(Value).Name, "")), x => x);
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float t) => new Vector2(Mathf.Lerp(a.x, b.x, t), Mathf.Lerp(a.y, b.y, t));
     }
 }
