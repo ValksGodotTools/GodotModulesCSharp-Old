ca3e420a93f4272ce2352507b0bca532d2f0d8ee
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-20 12:40:28 -0500

    remove comments

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -8,11 +8,6 @@ namespace GodotModules
 {
     public static class Utils
     {
-        /// <summary>
-        /// All logging should be done through here
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="color"></param>
         public static void Log(object obj, ConsoleColor color = ConsoleColor.Gray) 
         {
             Console.ForegroundColor = color;
@@ -20,36 +15,13 @@ namespace GodotModules
             Console.ResetColor();
         }
 
-        /// <summary>
-        /// Put a Dict<Key, Value> into a nice string format for printing to console.
-        /// </summary>
-        /// <param name="dict"></param>
-        /// <typeparam name="Key"></typeparam>
-        /// <typeparam name="Value"></typeparam>
-        /// <returns></returns>
         public static string StringifyDict<Key, Value>(Dictionary<Key, Value> dict) => string.Join(" ", dict.Select(x => $"{x.Key} {x.Value}"));
 
-        /// <summary>
-        /// Remap a value from one range to another
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="from1"></param>
-        /// <param name="to1"></param>
-        /// <param name="from2"></param>
-        /// <param name="to2"></param>
-        /// <returns></returns>
         public static float Remap(this float value, float from1, float to1, float from2, float to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
 
-        /// <summary>
-        /// Lerp between two Vector2's
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="t"></param>
-        /// <returns></returns>
         public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
         {
             float retX = Mathf.Lerp(a.x, b.x, t);
@@ -57,13 +29,6 @@ namespace GodotModules
             return new Vector2(retX, retY);
         }
 
-        /// <summary>
-        /// Creates instances of a type and puts them into a Dict<Key, Instance>
-        /// </summary>
-        /// <typeparam name="Key"></typeparam>
-        /// <typeparam name="Value"></typeparam>
-        /// <typeparam name="Namespace">Only add types from this namespace</typeparam>
-        /// <returns></returns>
         public static Dictionary<Key, Value> LoadInstances<Key, Value, Namespace>() =>
             Assembly.GetExecutingAssembly()
                 .GetTypes()
