314296a4ef970778c7c51a13a4474df113886550
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-22 21:37:54 -0500

    Add LogMs

--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace GodotModules
 {
@@ -21,6 +22,15 @@ namespace GodotModules
             Console.ForegroundColor = color;
             GD.Print(obj);
             Console.ResetColor();
+        }
+
+        public static void LogMs(Action code)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            code();
+            watch.Stop();
+            System.Console.WriteLine($"Took {watch.ElapsedMilliseconds} ms");
         }
 
         public static Vector2 Lerp(Vector2 a, Vector2 b, float t) => new Vector2(Mathf.Lerp(a.x, b.x, t), Mathf.Lerp(a.y, b.y, t));
