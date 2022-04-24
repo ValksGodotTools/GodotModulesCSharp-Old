aac01e5c4a75ee576f888c7d62cebccac1ce1d43
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-23 19:17:00 -0500

    simplify print for dict extension

--- Scripts/Utils/Extensions.cs
+++ Scripts/Utils/Extensions.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace GodotModules 
 {
     public static class Extensions 
     {
         public static string Print<T>(this List<T> list) => string.Join(", ", list);
-        public static string Print<TKey, TValue>(this Dictionary<TKey, TValue> dict) => string.Join(", ", dict.Select(x => $"{x.Key} {x.Value}"));
+        public static string Print<TKey, TValue>(this Dictionary<TKey, TValue> dict) => string.Join(", ", dict);
         public static string AddSpaceBeforeEachCapital(this string str) => string.Concat(str.Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
         public static string ToTitleCase(this string str) => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
         public static float Remap(this float value, float from1, float to1, float from2, float to2) => (value - from1) / (to1 - from1) * (to2 - from2) + from2;
