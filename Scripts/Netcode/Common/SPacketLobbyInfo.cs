7a2f6d2599d98287a0e4d4fc90e0fd58874cfd9a
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-24 13:52:52 -0500

    Keep track of peer ID client side

--- Scripts/Netcode/Common/SPacketLobbyInfo.cs
+++ Scripts/Netcode/Common/SPacketLobbyInfo.cs
@@ -6,16 +6,18 @@ namespace GodotModules.Netcode
     public class SPacketLobbyInfo : IPacketServer
     {
         public uint Id { get; set; }
-        public Dictionary<uint, string> Players { get; set; }
+        public Dictionary<uint, DataPlayer> Players { get; set; }
 
         public void Write(PacketWriter writer)
         {
             writer.Write((ushort)Id);
             writer.Write((ushort)Players.Count);
-            foreach (var player in Players)
+            foreach (var pair in Players)
             {
-                writer.Write((ushort)player.Key);
-                writer.Write((string)player.Value);
+                writer.Write((ushort)pair.Key); // id
+
+                var player = pair.Value;
+                writer.Write((string)player.Username);
             }
         }
 
@@ -23,24 +25,28 @@ namespace GodotModules.Netcode
         {
             Id = reader.ReadUInt16();
             var count = reader.ReadUInt16();
-            Players = new Dictionary<uint, string>();
+            Players = new Dictionary<uint, DataPlayer>();
             for (int i = 0; i < count; i++)
             {
                 var id = reader.ReadUInt16();
                 var name = reader.ReadString();
 
-                Players.Add(id, name);
+                Players.Add(id, new DataPlayer {
+                    Username = name,
+                    Ready = false
+                });
             }
         }
 
         public void Handle()
         {
+            ENetClient.PeerId = Id;
             ENetClient.Log($"{GameManager.Options.OnlineUsername} joined lobby with id {Id} also other players in lobby are {Players.Print()}");
 
             SceneLobby.AddPlayer(Id, GameManager.Options.OnlineUsername);
 
-            foreach (var player in Players)
-                SceneLobby.AddPlayer(player.Key, player.Value);
+            foreach (var pair in Players)
+                SceneLobby.AddPlayer(pair.Key, pair.Value.Username);
 
             SceneManager.ChangeScene("Lobby");
         }
