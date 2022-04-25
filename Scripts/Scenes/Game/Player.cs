afb9bf91148a54c199f0dd8c66f5872eee65246d
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-25 18:42:01 -0500

    Changes to player prefab

--- Scripts/Scenes/Game/Player.cs
+++ Scripts/Scenes/Game/Player.cs
@@ -4,11 +4,16 @@ namespace Game
 {
     public class Player : Sprite
     {
+        [Export] public readonly NodePath NodePathLabelUsername;
+
+        private Label LabelUsername { get; set; }
+
         [Export] public float Speed = 250f;
         public int Health = 100;
 
         public override void _Ready()
         {
+            LabelUsername = GetNode<Label>(NodePathLabelUsername);
             SetHealth(100);
         }
 
@@ -31,6 +36,11 @@ namespace Game
                 dir.y += 1;
 
             Position += dir * Speed * delta;
+        }
+
+        public void SetUsername(string username)
+        {
+            LabelUsername.Text = username;
         }
 
         public void SetHealth(int v)
