28a4e990e8ed70eb538fa628afda58b10f67b580
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-25 00:15:23 -0500

    add important note

--- Scripts/Netcode/Common/Packet/IPacketClient.cs
+++ Scripts/Netcode/Common/Packet/IPacketClient.cs
@@ -4,6 +4,9 @@ namespace GodotModules.Netcode
     {
         /// <summary>
         /// The packet handled server-side
+        /// 
+        /// IMPORTANT:
+        /// GameServer.Log() should be the only thing used to log values (System.Console.WriteLine() and Godot.GD.Print() will NOT work)
         /// </summary>
         /// <param name="peer">The client peer</param>
         void Handle(ENet.Peer peer);
