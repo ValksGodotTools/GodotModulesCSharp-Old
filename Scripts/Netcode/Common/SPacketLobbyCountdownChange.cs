0b10a3534b95074287f3cf6d0c33bde9ec4722d7
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-25 13:50:38 -0500

    SPacketLobbyCountdownChange.cs

---
+++ Scripts/Netcode/Common/SPacketLobbyCountdownChange.cs
@@ -0,0 +1,29 @@
+using GodotModules.Netcode.Client;
+
+namespace GodotModules.Netcode 
+{
+    public class SPacketLobbyCountdownChange : PacketServerPeerId
+    {
+        public bool CountdownRunning { get; set; }
+
+        public override void Write(PacketWriter writer)
+        {
+            base.Write(writer);
+            writer.Write(CountdownRunning);
+        }
+
+        public override void Read(PacketReader reader)
+        {
+            base.Read(reader);
+            CountdownRunning = reader.ReadBool();
+        }
+
+        public override void Handle()
+        {
+            if (CountdownRunning)
+                SceneLobby.StartGameCountdown();
+            else
+                SceneLobby.CancelGameCountdown();
+        }
+    }
+}
