21daacd3eb66e01f33079d870d5da9fc2841f94b
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-25 21:43:10 -0500

    change DataPlayer from struct to class

--- Scripts/Netcode/Common/Data/DataPlayer.cs
+++ Scripts/Netcode/Common/Data/DataPlayer.cs
@@ -1,9 +1,16 @@
+using Godot;
+
 namespace GodotModules.Netcode 
 {
-    public struct DataPlayer 
+    public class DataPlayer 
     {
         public string Username { get; set; }
         public bool Ready { get; set; }
         public bool Host { get; set; }
+        public bool PressedLeft { get; set; }
+        public bool PressedRight { get; set;}
+        public bool PressedDown { get; set; }
+        public bool PressedUp { get; set; }
+        public Vector2 Position { get; set; }
     }
 }
