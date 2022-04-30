0bf5e243bfb269600c4e58fc2ba04bf1aed04d32
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-04-30 17:09:56 -0500

    CPacketPlayerMovementDirections.cs

---
+++ Scripts/Netcode/Common/CPacketPlayerMovementDirections.cs
@@ -0,0 +1,31 @@
+using Godot;
+using GodotModules.Netcode.Server;
+using System;
+
+namespace GodotModules.Netcode
+{
+    public class CPacketPlayerMovementDirections : APacketClient
+    {
+        public Direction DirectionHorz { get; set; }
+        public Direction DirectionVert { get; set; }
+
+        public override void Write(PacketWriter writer)
+        {
+            writer.Write((byte)DirectionHorz);
+            writer.Write((byte)DirectionVert);
+        }
+
+        public override void Read(PacketReader reader)
+        {
+            DirectionHorz = (Direction)reader.ReadByte();
+            DirectionVert = (Direction)reader.ReadByte();
+        }
+
+        public override void Handle(ENet.Peer peer)
+        {
+            var player = GameServer.Players[peer.ID];
+            player.DirectionHorz = DirectionHorz;
+            player.DirectionVert = DirectionVert;
+        }
+    }
+}
