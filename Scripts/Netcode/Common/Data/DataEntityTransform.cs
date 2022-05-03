de66133a26829b436980b806eedd98582be1339c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-02 21:40:01 -0500

    DataEntityTransform

---
+++ Scripts/Netcode/Common/Data/DataEntityTransform.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace GodotModules.Netcode
+{
+    public class DataEntityTransform
+    {
+        public float Rotation { get; set; }
+        public Vector2 Position { get; set; }
+    }
+}
--- Scripts/Netcode/Common/Data/DataPlayer.cs
+++ Scripts/Netcode/Common/Data/DataPlayer.cs
@@ -2,14 +2,12 @@ using Godot;
 
 namespace GodotModules.Netcode
 {
-    public class DataPlayer
+    public class DataPlayer : DataEntityTransform
     {
         public string Username { get; set; }
         public bool Ready { get; set; }
         public bool Host { get; set; }
         public Direction DirectionHorz { get; set; }
         public Direction DirectionVert { get; set; }
-        public float Rotation { get; set; }
-        public Vector2 Position { get; set; }
     }
 }
