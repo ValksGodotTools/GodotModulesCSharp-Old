7325e4489bf781ea100e3db2652c5f804235dcc0
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-02 22:34:32 -0500

    update debug cmd

--- Scripts/Msc/Commands/CommandDebug.cs
+++ Scripts/Msc/Commands/CommandDebug.cs
@@ -1,58 +1,34 @@
 using System;
 using GodotModules.Netcode.Client;
+using GodotModules.Netcode.Server;
 
 namespace GodotModules
 {
     public class CommandDebug : Command
     {
         public CommandDebug() => Aliases = new string[] { "x" };
-
-        public static int SendReceiveDataInterval = 150;
-        private static List<ENetClient> dummyClients = new List<ENetClient>();
+        public static float TEST_VALUE = 0.1f;
 
         public override void Run(string[] args)
         {
             // debug command
             // do debug stuff here
-            if (args.Length == 0)
+            /*if (args.Length == 0)
                 return;
 
-            if (args[0] == "add")
-            {
-                /*var dummyClient = new ENetClient();
-                dummyClients.Add(dummyClient);
-                dummyClient.Start("127.0.0.1", 7777);
-                dummyClient.PingSent = DateTime.Now;
-
-                while (!dummyClient.IsConnected)
-                    await System.Threading.Tasks.Task.Delay(100);
-
-                await dummyClient.Send(Netcode.ClientPacketOpcode.Ping);*/
-            }
-
-            if (args[0] == "rem") 
-            {
-                dummyClients.ForEach(x => x.Stop());
-            }
-
-            /*if (!int.TryParse(args[0], out int result))
+            if (!int.TryParse(args[0], out int result))
                 return;
 
-            SendReceiveDataInterval = result;
-            Utils.Log("Set test value to " + result);
-
-            if (GameServer.Running)
+            if (NetworkManager.GameServer.Running)
             {
-                GameServer.EmitClientPositions.Stop();
-                GameServer.EmitClientPositions.Interval = SendReceiveDataInterval;
-                GameServer.EmitClientPositions.Start();
+                NetworkManager.GameServer.EmitClientTransforms.Stop();
+                NetworkManager.GameServer.EmitClientTransforms.SetDelay(result);
+                NetworkManager.GameServer.EmitClientTransforms.Start();
             }
 
-            if (GameClient.Running)
-            {
-                ClientPlayer.Timer.Stop();
-                ClientPlayer.Timer.Start(CommandDebug.SendReceiveDataInterval);
-            }*/
+            TEST_VALUE = result;
+
+            Utils.Log("Set test value to " + result);*/
         }
     }
 }
