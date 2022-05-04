c7c0968c625f3ab850617222d1750c3207226c84
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-04 01:44:38 -0500

    tidy up SceneGame.cs

--- Scripts/Scenes/Game/SceneGame.cs
+++ Scripts/Scenes/Game/SceneGame.cs
@@ -12,23 +12,26 @@ namespace Game
         public Label LabelPlayerHealth;
 
         private Dictionary<uint, OtherPlayer> Players;
-        private PrevCurQueue<Dictionary<byte, DataEntityTransform>> PlayerTransformQueue = new PrevCurQueue<Dictionary<byte, DataEntityTransform>>(ServerIntervals.PlayerTransforms);
+        public ClientPlayer Player { get; set; }
+        private PrevCurQueue<Dictionary<byte, DataEntityTransform>> PlayerTransformQueue { get; set; }
 
         private List<Sprite> Bullets = new List<Sprite>();
 
         public override void _Ready()
         {
-            var bullet = Prefabs.Bullet.Instance<Sprite>();
-            Bullets.Add(bullet);
-            AddChild(bullet);
+            LabelPlayerHealth = GetNode<Label>(NodePathLabelPlayerHealth);
 
             Players = new();
 
-            LabelPlayerHealth = GetNode<Label>(NodePathLabelPlayerHealth);
             Player = Prefabs.ClientPlayer.Instance<ClientPlayer>();
             Player.Position = Vector2.Zero;
 
             AddChild(Player);
+            PlayerTransformQueue = new PrevCurQueue<Dictionary<byte, DataEntityTransform>>(ServerIntervals.PlayerTransforms);
+
+            var bullet = Prefabs.Bullet.Instance<Sprite>();
+            Bullets.Add(bullet);
+            AddChild(bullet);
 
             // set game definitions
             ModLoader.Script.Globals["Player", "setHealth"] = (Action<int>)Player.SetHealth;
@@ -38,6 +41,36 @@ namespace Game
             if (NetworkManager.GameClient != null)
                 if (NetworkManager.GameClient.Running)
                     InitMultiplayerStuff();
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            ModLoader.Call("OnGameUpdate", delta);
+
+            if (SceneManager.PrevSceneName == "Menu") // singleplayer
+                return;
+            
+            foreach (var bullet in Bullets)
+                bullet.Position += new Vector2(0, -1f);
+
+            PlayerTransformQueue.UpdateProgress(delta);
+
+            if (PlayerTransformQueue.NotReady)
+                return;
+
+            foreach (var pair in PlayerTransformQueue.Current)
+            {
+                if (!Players.ContainsKey(pair.Key)) // TODO: Find more optimal approach for checking this
+                    continue;
+
+                var player = Players[pair.Key];
+
+                var prev = PlayerTransformQueue.Previous[pair.Key];
+                var cur = pair.Value;
+
+                player.Position = Utils.Lerp(prev.Position, cur.Position, PlayerTransformQueue.Progress);
+                player.PlayerSprite.Rotation = Utils.LerpAngle(player.PlayerSprite.Rotation, cur.Rotation, 0.05f);
+            }
         }
 
         public override async void _Input(InputEvent @event)
@@ -60,12 +93,7 @@ namespace Game
             }
         }
 
-        public void UpdatePlayerPositions(Dictionary<byte, DataEntityTransform> playerTransforms) // TODO: Move stat ic somewhere else
-        {
-            PlayerTransformQueue.Add(playerTransforms);
-        }
-
-        public ClientPlayer Player { get; set; }
+        public void UpdatePlayerPositions(Dictionary<byte, DataEntityTransform> playerTransforms) => PlayerTransformQueue.Add(playerTransforms);
 
         public void RemovePlayer(byte id)
         {
@@ -91,36 +119,6 @@ namespace Game
                     AddChild(otherPlayer);
                     otherPlayer.SetUsername(pair.Value);
                 });
-        }
-
-        public override void _PhysicsProcess(float delta)
-        {
-            ModLoader.Call("OnGameUpdate", delta);
-
-            if (SceneManager.PrevSceneName == "Menu") // singleplayer
-                return;
-            
-            foreach (var bullet in Bullets)
-                bullet.Position += new Vector2(0, -1f);
-
-            PlayerTransformQueue.UpdateProgress(delta);
-
-            if (PlayerTransformQueue.NotReady)
-                return;
-
-            foreach (var pair in PlayerTransformQueue.Current)
-            {
-                if (!Players.ContainsKey(pair.Key))
-                    continue;
-
-                var player = Players[pair.Key];
-
-                var prev = PlayerTransformQueue.Previous[pair.Key];
-                var cur = pair.Value;
-
-                player.Position = Utils.Lerp(prev.Position, cur.Position, PlayerTransformQueue.Progress);
-                player.PlayerSprite.Rotation = Utils.LerpAngle(player.PlayerSprite.Rotation, cur.Rotation, 0.05f);
-            }
         }
 
         public override void Cleanup()
