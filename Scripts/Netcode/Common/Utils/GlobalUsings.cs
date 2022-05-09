a4dcd1acb44a0c23be781672197d4207686b6514
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-09 13:00:23 -0500

    Update GlobalUsings

--- Scripts/Netcode/Common/Utils/GlobalUsings.cs
+++ Scripts/Netcode/Common/Utils/GlobalUsings.cs
@@ -1,4 +1,6 @@
 global using System.Collections.Generic;
+global using System.Collections.Concurrent;
 global using System.Linq;
+global using System.Threading.Tasks;
 global using GodotModules;
 global using GodotModules.Netcode;
