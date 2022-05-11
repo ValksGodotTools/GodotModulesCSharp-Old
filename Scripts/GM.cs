833a91ce80bc0cc4e4721396c8b882ea32d74da5
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-10 21:56:54 -0500

    Implement thread-safe Logger

---
+++ Scripts/GM.cs
@@ -0,0 +1,47 @@
+global using GodotModules;
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.Concurrent;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Threading.Tasks;
+global using System.Linq;
+
+using Godot;
+
+namespace GodotModules
+{
+    public class GM : Node
+    {
+        private static Logger _logger = new Logger();
+
+        public override void _Ready()
+        {
+            Log("Hello world");
+            LogWarning("Warning");
+            LogDebug("Debug");
+            LogErr(new Exception("Exception"));
+            LogTodo("Has yet to be done");
+        }
+
+        public override void _Process(float delta)
+        {
+            _logger.Update();
+        }
+
+        public override void _Notification(int what)
+        {
+            if (what == MainLoop.NotificationWmQuitRequest)
+            {
+                GetTree().SetAutoAcceptQuit(false);
+                GetTree().Quit();
+            }
+        }
+
+        public static void Log(object v, ConsoleColor c = ConsoleColor.Gray) => _logger.Log(v, c);
+        public static void LogWarning(object v, ConsoleColor c = ConsoleColor.Yellow) => _logger.LogWarning(v, c);
+        public static void LogDebug(object v, bool trace = true, ConsoleColor c = ConsoleColor.Magenta, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0) => _logger.LogDebug(v, c, trace, filePath, lineNumber);
+        public static void LogErr(Exception e, ConsoleColor c = ConsoleColor.Red) => _logger.LogErr(e, c);
+        public static void LogTodo(object v, ConsoleColor c = ConsoleColor.White) => _logger.LogTodo(v, c);
+    }
+}
---
+++ Scripts/Logger.cs
@@ -0,0 +1,89 @@
+using Godot;
+
+namespace GodotModules
+{
+    public class Logger
+    {
+        private ConcurrentQueue<LogInfo> _messages = new ConcurrentQueue<LogInfo>();
+
+        public void LogErr(Exception e, ConsoleColor c) => _messages.Enqueue(new LogInfo(LoggerOpcode.Exception, $"[Error]: {e.Message}\n{e.StackTrace}", c));
+        public void LogDebug(object v, ConsoleColor c, bool trace, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0) => _messages.Enqueue(new LogInfo(LoggerOpcode.Debug, new LogMessageDebug($"[Debug]: {v}", trace, $"   at {filePath.Substring(filePath.IndexOf("Scripts\\"))} line:{lineNumber}"), c));
+        public void LogTodo(object v, ConsoleColor c) => Log($"[Todo]: {v}", c);
+        public void LogWarning(object v, ConsoleColor c) => Log($"[Warning]: {v}", c);
+        public void Log(object v, ConsoleColor c) => _messages.Enqueue(new LogInfo(LoggerOpcode.Message, $"{v}", c));
+
+        public void Update()
+        {
+            if (_messages.TryDequeue(out LogInfo result))
+            {
+                switch (result.Opcode)
+                {
+                    case LoggerOpcode.Message:
+                        Print((string)result.Data, result.Color);
+                        Console.ResetColor();
+                        break;
+
+                    case LoggerOpcode.Exception:
+                        PrintErr((string)result.Data, result.Color);
+                        Console.ResetColor();
+                        break;
+
+                    case LoggerOpcode.Debug:
+                        var data = (LogMessageDebug)result.Data;
+                        Print(data.Message, result.Color);
+                        if (data.Trace)
+                            Print(data.TracePath, ConsoleColor.DarkGray);
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+
+        private void Print(object v, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            GD.Print(v);
+        }
+
+        private void PrintErr(object v, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            GD.PrintErr(v);
+        }
+    }
+
+    public struct LogInfo
+    {
+        public LoggerOpcode Opcode { get; set; }
+        public object Data { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public LogInfo(LoggerOpcode opcode, object data, ConsoleColor color = ConsoleColor.Gray) 
+        {
+            Opcode = opcode;
+            Data = data;
+            Color = color;
+        }
+    }
+
+    public struct LogMessageDebug 
+    {
+        public string Message { get; set; }
+        public bool Trace { get; set; }
+        public string TracePath { get; set; }
+
+        public LogMessageDebug(string message, bool trace = true, string tracePath = "")
+        {
+            Message = message;
+            Trace = trace;
+            TracePath = tracePath;
+        }
+    }
+
+    public enum LoggerOpcode 
+    {
+        Message,
+        Exception,
+        Debug
+    }
+}
