108c5f892deb8de18ca2de2ca16ad041b18967e2
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-11 17:41:29 -0500

    Link menu buttons

---
+++ Scripts/UI/UIMenu.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public class UIMenu : Node
+{
+    private void _on_Play_pressed()
+    {
+
+    }
+
+    private void _on_Multiplayer_pressed()
+    {
+
+    }
+
+    private void _on_Options_pressed()
+    {
+
+    }
+
+    private void _on_Mods_pressed()
+    {
+
+    }
+
+    private void _on_Credits_pressed()
+    {
+
+    }
+
+    private void _on_Quit_pressed()
+    {
+
+    }
+
+    private void _on_Discord_pressed()
+    {
+
+    }
+
+    private void _on_GitHub_pressed()
+    {
+        
+    }
+}
