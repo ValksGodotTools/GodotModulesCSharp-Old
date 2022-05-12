c473fd03f71e62a90d1ae5f13a1bb6809a2ee2a8
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-12 01:26:25 -0500

    wrap with namespace

--- Scripts/UI/UICredits.cs
+++ Scripts/UI/UICredits.cs
@@ -1,23 +1,27 @@
 using Godot;
 using System;
 
-public class UICredits : Node
+namespace GodotModules
 {
-    [Export] public readonly NodePath NodePathTween;
-    [Export] public readonly NodePath NodePathCreditsContent;
-    private Tween Tween { get; set; }
-    private Control CreditsContent { get; set; }
-
-    public override void _Ready()
+    public class UICredits : Node
     {
-        CreditsContent = GetNode<Control>(NodePathCreditsContent);
-        Tween = GetNode<Tween>(NodePathTween);
+        [Export] public readonly NodePath NodePathTween;
+        [Export] public readonly NodePath NodePathCreditsContent;
+        private Tween Tween { get; set; }
+        private Control CreditsContent { get; set; }
 
-        var duration = (CreditsContent.RectSize.y / OS.WindowSize.y) * 50f;
+        public override void _Ready()
+        {
+            CreditsContent = GetNode<Control>(NodePathCreditsContent);
+            Tween = GetNode<Tween>(NodePathTween);
 
-        Tween.InterpolateProperty(CreditsContent, "rect_position:y", OS.WindowSize.y, -CreditsContent.RectSize.y, duration);
-        Tween.Start();
+            var duration = (CreditsContent.RectSize.y / OS.WindowSize.y) * 50f;
+
+            Tween.InterpolateProperty(CreditsContent, "rect_position:y", OS.WindowSize.y, -CreditsContent.RectSize.y, duration);
+            Tween.Start();
+        }
+
+        private async void _on_Tween_tween_completed(object obj, NodePath key) => await GM.ChangeScene("Menu");
     }
-
-    private async void _on_Tween_tween_completed(object obj, NodePath key) => await GM.ChangeScene("Menu");
 }
+
--- Scripts/UI/UIMenu.cs
+++ Scripts/UI/UIMenu.cs
@@ -1,13 +1,16 @@
 using Godot;
 
-public class UIMenu : Node
+namespace GodotModules
 {
-    private async void _on_Play_pressed() => await GM.ChangeScene("Game");
-    private async void _on_Multiplayer_pressed() => await GM.ChangeScene("GameServers");
-    private async void _on_Options_pressed() => await GM.ChangeScene("Options");
-    private async void _on_Mods_pressed() => await GM.ChangeScene("Mods");
-    private async void _on_Credits_pressed() => await GM.ChangeScene("Credits");
-    private void _on_Quit_pressed() => GM.Quit();
-    private void _on_Discord_pressed() => OS.ShellOpen("https://discord.gg/866cg8yfxZ");
-    private void _on_GitHub_pressed() => OS.ShellOpen("https://github.com/GodotModules/GodotModulesCSharp");
+    public class UIMenu : Node
+    {
+        private async void _on_Play_pressed() => await GM.ChangeScene("Game");
+        private async void _on_Multiplayer_pressed() => await GM.ChangeScene("GameServers");
+        private async void _on_Options_pressed() => await GM.ChangeScene("Options");
+        private async void _on_Mods_pressed() => await GM.ChangeScene("Mods");
+        private async void _on_Credits_pressed() => await GM.ChangeScene("Credits");
+        private void _on_Quit_pressed() => GM.Quit();
+        private void _on_Discord_pressed() => OS.ShellOpen("https://discord.gg/866cg8yfxZ");
+        private void _on_GitHub_pressed() => OS.ShellOpen("https://github.com/GodotModules/GodotModulesCSharp");
+    }
 }
