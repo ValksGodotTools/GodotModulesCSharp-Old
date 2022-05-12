a1b13887bae612cf9c0f3547118b8a412fdcb6bc
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-12 11:39:06 -0500

    Prefabs.cs

--- Scripts/Prefabs.cs
+++ Scripts/Prefabs.cs
@@ -1,7 +1,11 @@
+using Godot;
+
 namespace GodotModules 
 {
     public static class Prefabs 
     {
+        public static PackedScene UIHotkey = LoadPrefab("UIHotkey");
 
+        private static PackedScene LoadPrefab(string prefab) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{prefab}.tscn");
     }
 }
--- Scripts/UI/UIControls.cs
+++ Scripts/UI/UIControls.cs
@@ -5,11 +5,9 @@ namespace GodotModules
 {
     public class UIControls : Node
     {
-        private PackedScene PrefabUIHotkey = ResourceLoader.Load<PackedScene>("res://Scenes/Prefabs/UIHotkey.tscn");
-
         public override void _Ready()
         {
-            var hotkey = PrefabUIHotkey.Instance<UIHotkey>();
+            var hotkey = Prefabs.UIHotkey.Instance<UIHotkey>();
             hotkey.Init("player_move_left");
             AddChild(hotkey);
         }
