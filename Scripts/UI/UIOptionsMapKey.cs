43e7b6e2fd5d2082946cd4dbdc49c4e294c2d1e4
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-12 01:26:03 -0500

    UIOptionsMapKey.cs

---
+++ Scripts/UI/UIOptionsMapKey.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+namespace GodotModules
+{
+    public class UIOptionsMapKey : Button
+    {
+        [Export] public readonly string Action;
+
+        private string _hotkey = "";
+        private bool _waitingForHotkey;
+
+        public override void _Ready()
+        {
+            var key = (InputEvent)InputMap.GetActionList(Action)[0];
+            Text = key.AsText();
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (@event is InputEventKey keyEvent && !keyEvent.Pressed && _waitingForHotkey)
+            {
+                _waitingForHotkey = false;
+                _hotkey = @event.AsText();
+                Text = @event.AsText();
+                GM.SetHotkey(Action, keyEvent);
+            }
+
+            if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+                if (!GetGlobalRect().HasPoint(mouseEvent.Position))
+                    LostFocus();
+        }
+
+        private void _on_Btn_pressed()
+        {
+            _waitingForHotkey = true;
+            Text = "...";
+        }
+
+        private void _on_Btn_focus_exited() => LostFocus();
+
+        private void LostFocus() 
+        {
+            _waitingForHotkey = false;
+            Text = _hotkey;
+        }
+    }
+}
