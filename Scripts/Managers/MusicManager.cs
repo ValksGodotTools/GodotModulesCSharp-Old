d868e43a99974764dc9909268d273ebff453a138
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-13 18:02:12 -0500

    MusicManager

---
+++ Scripts/Managers/MusicManager.cs
@@ -0,0 +1,46 @@
+using Godot;
+
+namespace GodotModules 
+{
+    public class MusicManager 
+    {
+        private Dictionary<string, AudioStream> _tracks = new Dictionary<string, AudioStream>();
+        private AudioStreamPlayer _audioStreamPlayer;
+        private OptionsManager _optionsManager;
+
+        public MusicManager(AudioStreamPlayer audioStreamPlayer, OptionsManager optionsManager)
+        {
+            _audioStreamPlayer = audioStreamPlayer;
+            _optionsManager = optionsManager;
+            _audioStreamPlayer.VolumeDb = optionsManager.Options.MusicVolume;
+        }
+
+        public void LoadTrack(string name, string path)
+        {
+            if (!path.Contains("res://"))
+                path = "res://" + path;
+
+            _tracks[name.ToLower()] = ResourceLoader.Load<AudioStream>(path);
+        }
+
+        public void PlayTrack(string name)
+        {
+            _audioStreamPlayer.Stream = _tracks[name.ToLower()];
+            _audioStreamPlayer.Playing = true;
+        }
+
+        public void SetVolume(float volume) => SetVolumeValue(volume.Remap(0, 100, -40, 0)); // expects a value from 0 to 100
+
+        public void SetVolumeValue(float volume) // expects a value from -40 to 0
+        {
+            if (volume <= -40)
+                volume = -80; // can't go lower than this (this essentially mutes the track)
+
+            _audioStreamPlayer.VolumeDb = volume;
+            _optionsManager.Options.MusicVolume = volume;
+        }
+
+        public void Pause() => _audioStreamPlayer.Playing = false;
+        public void Resume() => _audioStreamPlayer.Playing = true;
+    }
+}
