85e3a59abcd6fdf327122a0a2a1c42c906e7edc6
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-13 13:23:20 -0500

    ScrollContainer / Reset Hotkeys btn at bottom

--- Scripts/UI/UIControls.cs
+++ Scripts/UI/UIControls.cs
@@ -5,6 +5,9 @@ namespace GodotModules
 {
     public class UIControls : Control
     {
+        [Export] public readonly NodePath NodePathVBoxHotkeys;
+        private Control _vboxHotkeys;
+
         private HotkeyManager _hotkeyManager { get; set; }
         private Dictionary<string, UIHotkey> _uiHotkeys = new Dictionary<string, UIHotkey>();
 
@@ -15,11 +18,12 @@ namespace GodotModules
 
         public override void _Ready()
         {
+            _vboxHotkeys = GetNode<Control>(NodePathVBoxHotkeys);
             foreach (var action in _hotkeyManager.Hotkeys.Keys.OrderBy(x => x).ToList()) 
             {
                 var hotkeyInstance = Prefabs.UIHotkey.Instance<UIHotkey>();
                 hotkeyInstance.Init(_hotkeyManager, action);
-                AddChild(hotkeyInstance);
+                _vboxHotkeys.AddChild(hotkeyInstance);
                 _uiHotkeys[action] = hotkeyInstance;
             }
         }
