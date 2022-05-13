7631a088eb1656c702cbdcb9a444efd66d61b664
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-13 16:50:37 -0500

    set focus left for all children

--- Scripts/UI/Options/UIOptions.cs
+++ Scripts/UI/Options/UIOptions.cs
@@ -7,6 +7,13 @@ namespace GodotModules
     {
         private static OptionSection _currentSection = OptionSection.Game;
 
+        [Export] public readonly NodePath NodePathBtnGame;
+        [Export] public readonly NodePath NodePathBtnVideo;
+        [Export] public readonly NodePath NodePathBtnDisplay;
+        [Export] public readonly NodePath NodePathBtnAudio;
+        [Export] public readonly NodePath NodePathBtnControls;
+        [Export] public readonly NodePath NodePathBtnMultiplayer;
+
         [Export] public readonly NodePath NodePathOptionsGame;
         [Export] public readonly NodePath NodePathOptionsVideo;
         [Export] public readonly NodePath NodePathOptionsDisplay;
@@ -14,6 +21,12 @@ namespace GodotModules
         [Export] public readonly NodePath NodePathOptionsControls;
         [Export] public readonly NodePath NodePathOptionsMultiplayer;
 
+        private Button _btnGame;
+        private Button _btnVideo;
+        private Button _btnDisplay;
+        private Button _btnAudio;
+        private Button _btnControls;
+        private Button _btnMultiplayer;
         private Dictionary<OptionSection, Control> _optionSections;
 
         public void PreInit(HotkeyManager hotkeyManager)
@@ -31,15 +44,61 @@ namespace GodotModules
 
         public override void _Ready()
         {
+            _btnGame = GetNode<Button>(NodePathBtnGame);
+            _btnVideo = GetNode<Button>(NodePathBtnVideo);
+            _btnDisplay = GetNode<Button>(NodePathBtnDisplay);
+            _btnAudio = GetNode<Button>(NodePathBtnAudio);
+            _btnControls = GetNode<Button>(NodePathBtnControls);
+            _btnMultiplayer = GetNode<Button>(NodePathBtnMultiplayer);
+            _btnGame.GrabFocus();
             ShowSection(_currentSection);
+
+            foreach (var pair in _optionSections)
+            {
+                var section = pair.Value;
+
+                switch (pair.Key)
+                {
+                    case OptionSection.Game:
+                        SetFocusNeighborLeft(section, _btnGame);
+                        break;
+                    case OptionSection.Video:
+                        SetFocusNeighborLeft(section, _btnVideo);
+                        break;
+                    case OptionSection.Display:
+                        SetFocusNeighborLeft(section, _btnDisplay);
+                        break;
+                    case OptionSection.Audio:
+                        SetFocusNeighborLeft(section, _btnAudio);
+                        break;
+                    case OptionSection.Controls:
+                        SetFocusNeighborLeft(section, _btnControls);
+                        break;
+                    case OptionSection.Multiplayer:
+                        SetFocusNeighborLeft(section, _btnMultiplayer);
+                        break;
+                }
+            }
         }
 
-        private void _on_Game_pressed() => ShowSection(OptionSection.Game);
-        private void _on_Video_pressed() => ShowSection(OptionSection.Video);
-        private void _on_Display_pressed() => ShowSection(OptionSection.Display);
-        private void _on_Audio_pressed() => ShowSection(OptionSection.Audio);
-        private void _on_Controls_pressed() => ShowSection(OptionSection.Controls);
-        private void _on_Multiplayer_pressed() => ShowSection(OptionSection.Multiplayer);
+        private void SetFocusNeighborLeft(Control parent, Control target) => GetControlChildren(parent, (child) => child.FocusNeighbourLeft = target.GetPath());
+
+        private void GetControlChildren(Node node, Action<Control> code)
+        {
+            foreach (Node child in node.GetChildren())
+            {
+                if (child is Control control)
+                    code(control);
+                GetControlChildren(child, code);
+            }
+        }
+
+        private void _on_Game_focus_entered() => ShowSection(OptionSection.Game);
+        private void _on_Video_focus_entered() => ShowSection(OptionSection.Video);
+        private void _on_Display_focus_entered() => ShowSection(OptionSection.Display);
+        private void _on_Audio_focus_entered() => ShowSection(OptionSection.Audio);
+        private void _on_Controls_focus_entered() => ShowSection(OptionSection.Controls);
+        private void _on_Multiplayer_focus_entered() => ShowSection(OptionSection.Multiplayer);
 
         private void ShowSection(OptionSection section)
         {
@@ -50,7 +109,7 @@ namespace GodotModules
             _currentSection = section;
         }
 
-        private enum OptionSection 
+        private enum OptionSection
         {
             Game,
             Video,
