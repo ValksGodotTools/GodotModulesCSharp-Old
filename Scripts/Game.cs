efdd208fb975aba88e6b3c6ff401041b5a3fb4be
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-15 13:08:01 -0500

    Organize managers

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -18,63 +18,21 @@ namespace GodotModules
     {
         [Export] public readonly NodePath NodePathAudioStreamPlayer;
         [Export] public readonly NodePath NodePathWebRequestList;
-        private AudioStreamPlayer _audioStreamPlayer;
-        private Node _webRequestList;
 
-        private HotkeyManager _hotkeyManager;
-        private SystemFileManager _systemFileManager;
-        private SceneManager _sceneManager;
         private OptionsManager _optionsManager;
-        private MusicManager _musicManager;
-        private NetworkManager _networkManager;
         private TokenManager _tokenManager;
+        private NetworkManager _networkManager;
+        private SceneManager _sceneManager;
         private WebManager _webManager;
+        private MusicManager _musicManager;
 
         public override async void _Ready()
         {
-            _audioStreamPlayer = GetNode<AudioStreamPlayer>(NodePathAudioStreamPlayer);
-            _webRequestList = GetNode<Node>(NodePathWebRequestList);
-
-            _systemFileManager = new();
-            _hotkeyManager = new(_systemFileManager);
-            _optionsManager = new(_systemFileManager, _hotkeyManager);
-            _musicManager = new(_audioStreamPlayer, _optionsManager);
-
-            _tokenManager = new();
-            var _webRequests = new WebRequests(_webRequestList);
-            _webManager = new WebManager(_webRequests, _optionsManager.Options.WebServerAddress);
-
-            _sceneManager = new(this, new GodotFileManager(), _hotkeyManager);
-            _sceneManager.PreInit[Scene.Menu] = (scene) =>
-            {
-                var menu = (UIMenu)scene;
-                menu.PreInit(_sceneManager);
-            };
-            _sceneManager.PreInit[Scene.Options] = (scene) =>
-            {
-                var options = (UIOptions)scene;
-                options.PreInit(_hotkeyManager, _optionsManager, _musicManager, _webManager, _sceneManager, _tokenManager);
-            };
-            _sceneManager.PreInit[Scene.Credits] = (scene) =>
-            {
-                var credits = (UICredits)scene;
-                credits.PreInit(_sceneManager);
-            };
-            _sceneManager.EscPressed[Scene.Credits] = async () => await _sceneManager.ChangeScene(Scene.Menu);
-            _sceneManager.EscPressed[Scene.GameServers] = async () => await _sceneManager.ChangeScene(Scene.Menu);
-            _sceneManager.EscPressed[Scene.Mods] = async () => await _sceneManager.ChangeScene(Scene.Menu);
-            _sceneManager.EscPressed[Scene.Options] = async () => {
-                _tokenManager.Cancel("check_connection");
-                await _sceneManager.ChangeScene(Scene.Menu);
-            };
-            _sceneManager.EscPressed[Scene.Lobby] = async () => await _sceneManager.ChangeScene(Scene.GameServers);
-            _sceneManager.EscPressed[Scene.Game] = async () => await _sceneManager.ChangeScene(Scene.Menu);
-            await _sceneManager.InitAsync();
+            await InitManagers();
 
             _musicManager.LoadTrack("Menu", "Audio/Music/Unsolicited trailer music loop edit.wav");
             _musicManager.PlayTrack("Menu");
 
-            _networkManager = new();
             _networkManager.StartServer(25565, 100);
             _networkManager.StartClient("127.0.0.1", 25565);
 
@@ -111,6 +69,55 @@ namespace GodotModules
             await _networkManager.Cleanup();
             _tokenManager.Cleanup();
             GetTree().Quit();
+        }
+
+        private async Task InitManagers()
+        {
+            var systemFileManager = new SystemFileManager();
+            var hotkeyManager = new HotkeyManager(systemFileManager);
+            _optionsManager = new(systemFileManager, hotkeyManager);
+            _webManager = new(new WebRequests(GetNode<Node>(NodePathWebRequestList)), _optionsManager.Options.WebServerAddress);
+            _musicManager = new(GetNode<AudioStreamPlayer>(NodePathAudioStreamPlayer), _optionsManager);
+            
+            _tokenManager = new();
+            await InitSceneManager(hotkeyManager);
+
+            _networkManager = new();
+        }
+
+        private async Task InitSceneManager(HotkeyManager hotkeyManager)
+        {
+            _sceneManager = new(this, new GodotFileManager(), hotkeyManager);
+
+            // Pre Initialization
+            _sceneManager.PreInit[Scene.Menu] = (scene) =>
+            {
+                var menu = (UIMenu)scene;
+                menu.PreInit(_sceneManager);
+            };
+            _sceneManager.PreInit[Scene.Options] = (scene) =>
+            {
+                var options = (UIOptions)scene;
+                options.PreInit(hotkeyManager, _optionsManager, _musicManager, _webManager, _sceneManager, _tokenManager);
+            };
+            _sceneManager.PreInit[Scene.Credits] = (scene) =>
+            {
+                var credits = (UICredits)scene;
+                credits.PreInit(_sceneManager);
+            };
+
+            // Esc Pressed
+            _sceneManager.EscPressed[Scene.Credits] = async () => await _sceneManager.ChangeScene(Scene.Menu);
+            _sceneManager.EscPressed[Scene.GameServers] = async () => await _sceneManager.ChangeScene(Scene.Menu);
+            _sceneManager.EscPressed[Scene.Mods] = async () => await _sceneManager.ChangeScene(Scene.Menu);
+            _sceneManager.EscPressed[Scene.Options] = async () => {
+                _tokenManager.Cancel("check_connection");
+                await _sceneManager.ChangeScene(Scene.Menu);
+            };
+            _sceneManager.EscPressed[Scene.Lobby] = async () => await _sceneManager.ChangeScene(Scene.GameServers);
+            _sceneManager.EscPressed[Scene.Game] = async () => await _sceneManager.ChangeScene(Scene.Menu);
+
+            await _sceneManager.InitAsync();
         }
     }
 }
