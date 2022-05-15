217c85d2432849229418721cd1336c4ff47a3f9c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-15 01:44:37 -0500

    check web server status logic

--- Scripts/UI/Options/UIOptionsMultiplayer.cs
+++ Scripts/UI/Options/UIOptionsMultiplayer.cs
@@ -7,21 +7,34 @@ namespace GodotModules
     {
         [Export] public readonly NodePath NodePathOnlineUsername;
         [Export] public readonly NodePath NodePathWebServerAddress;
+        [Export] public readonly NodePath NodePathBtnCheckConnection;
+        [Export] public readonly NodePath NodePathWebServerStatus;
         private LineEdit _onlineUsername;
         private LineEdit _webServerAddress;
-        private OptionsManager _optionsManager;
+        private Button _btnCheckConnection;
+        private Label _webServerStatus;
 
-        public void PreInit(OptionsManager optionsManager)
+        private OptionsManager _optionsManager;
+        private WebManager _webManager;
+        private SceneManager _sceneManager;
+
+        public void PreInit(OptionsManager optionsManager, WebManager webManager, SceneManager sceneManager)
         {
             _optionsManager = optionsManager;
+            _webManager = webManager;
+            _sceneManager = sceneManager;
         }
 
         public override void _Ready()
         {
             _onlineUsername = GetNode<LineEdit>(NodePathOnlineUsername);
             _webServerAddress = GetNode<LineEdit>(NodePathWebServerAddress);
+            _btnCheckConnection = GetNode<Button>(NodePathBtnCheckConnection);
+            _webServerStatus = GetNode<Label>(NodePathWebServerStatus);
+
             _onlineUsername.Text = _optionsManager.Options.OnlineUsername;
             _webServerAddress.Text = _optionsManager.Options.WebServerAddress;
+            SetWebServerStatus(_webManager.ConnectionAlive);
         }
 
         private void _on_OnlineUsername_text_changed(string newText)
@@ -31,7 +44,24 @@ namespace GodotModules
 
         private void _on_WebServerAddress_text_changed(string newText)
         {
+            _webManager.Ip = newText;
             _optionsManager.Options.WebServerAddress = newText;
         }
+
+        private async void _on_Check_Connection_pressed()
+        {
+            _btnCheckConnection.Disabled = true;
+            _webServerStatus.Text = "Checking...";
+
+            await _webManager.CheckConnectionAsync();
+
+            if (_sceneManager.CurScene == Scene.Options) // player could go to menu while this task is still executing
+            {
+                _btnCheckConnection.Disabled = false;
+                SetWebServerStatus(_webManager.ConnectionAlive);
+            }
+        }
+
+        private void SetWebServerStatus(bool online) => _webServerStatus.Text = $"Connection to web server {(online ? "successful" : "failed")}";
     }
 }
