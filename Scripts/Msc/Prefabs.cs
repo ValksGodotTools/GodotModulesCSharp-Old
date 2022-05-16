32e20caed2bea01276b523dec8dc067a4b7c62a9
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-16 00:11:10 -0500

    Update prefabs

--- Scripts/Msc/Prefabs.cs
+++ Scripts/Msc/Prefabs.cs
@@ -4,9 +4,15 @@ namespace GodotModules
 {
     public static class Prefabs 
     {
+        // Prefabs
         public readonly static PackedScene UIHotkey = LoadPrefab("UIHotkey");
         public readonly static PackedScene UIErrorNotifier = LoadPrefab("UIErrorNotifier");
 
+        // Popups
+        public readonly static PackedScene UIPopupMessage = LoadPopupPrefab("UIPopupMessage");
+        public readonly static PackedScene UIPopupError = LoadPopupPrefab("UIPopupError");
+
+        private static PackedScene LoadPopupPrefab(string popup) => LoadPrefab($"Popups/{popup}");
         private static PackedScene LoadPrefab(string prefab) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{prefab}.tscn");
     }
 }
