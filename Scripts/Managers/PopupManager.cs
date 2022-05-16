bd512f530dd74c32e555eb7aefe28861ab1fb6b4
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-16 01:00:01 -0500

    popups do not popup all at once, they are ordered

--- Scripts/Managers/PopupManager.cs
+++ Scripts/Managers/PopupManager.cs
@@ -1,24 +1,44 @@
 using Godot;
-using System;
 
 namespace GodotModules
 {
     public class PopupManager : Control
     {
-        public void SpawnPopupMessage(string message, string title = "") 
+        private Queue<WindowDialog> _popups = new();
+
+        public void SpawnPopupMessage(string message, string title = "")
         {
             var popupMessage = Prefabs.UIPopupMessage.Instance<UIPopupMessage>();
-            popupMessage.PreInit(message, title);
+
+            popupMessage.PreInit(this, message, title);
             AddChild(popupMessage);
-            popupMessage.PopupCentered();
+
+            if (_popups.Count == 0)
+                popupMessage.PopupCentered();
+
+            _popups.Enqueue(popupMessage);
         }
 
-        public void SpawnPopupError(Exception exception, string title = "") 
+        public void SpawnPopupError(Exception exception, string title = "")
         {
             var popupError = Prefabs.UIPopupError.Instance<UIPopupError>();
-            popupError.PreInit(exception, title);
+
+            popupError.PreInit(this, exception, title);
             AddChild(popupError);
-            popupError.PopupCentered();
+
+            if (_popups.Count == 0)
+                popupError.PopupCentered();
+                
+            _popups.Enqueue(popupError);
+        }
+
+        public void SpawnNextPopup() 
+        {
+            _popups.Dequeue();
+            if (_popups.Count == 0) 
+                return;
+            var popup = _popups.Peek();
+            popup.PopupCentered();
         }
     }
 }
