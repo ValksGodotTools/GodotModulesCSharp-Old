a0a7bfea244bd2dbe5c9a4b993ab42346f9a803a
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-17 15:01:50 -0500

    cleanup Prefabs.cs

--- Scripts/Msc/Prefabs.cs
+++ Scripts/Msc/Prefabs.cs
@@ -4,15 +4,22 @@ namespace GodotModules
 {
     public static class Prefabs 
     {
-        // Prefabs
-        public readonly static PackedScene UIHotkey = LoadPrefab("UIHotkey");
-        public readonly static PackedScene UIErrorNotifier = LoadPrefab("UIErrorNotifier");
+        // UI
+        public readonly static PackedScene UIHotkey = LoadUI("UIHotkey");
+        public readonly static PackedScene UIErrorNotifier = LoadUI("UIErrorNotifier");
 
         // Popups
-        public readonly static PackedScene UIPopupMessage = LoadPopupPrefab("UIPopupMessage");
-        public readonly static PackedScene UIPopupError = LoadPopupPrefab("UIPopupError");
+        public readonly static PackedScene UIPopupMessage = LoadPopup("UIPopupMessage");
+        public readonly static PackedScene UIPopupError = LoadPopup("UIPopupError");
 
-        private static PackedScene LoadPopupPrefab(string popup) => LoadPrefab($"Popups/{popup}");
-        private static PackedScene LoadPrefab(string prefab) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{prefab}.tscn");
+        // Game
+        public readonly static PackedScene Player = LoadGame("Player");
+        public readonly static PackedScene OtherPlayer = LoadGame("OtherPlayer");
+        public readonly static PackedScene Enemy = LoadGame("Enemy");
+
+        private static PackedScene LoadGame(string game) => Load($"Game/{game}");
+        private static PackedScene LoadPopup(string popup) => LoadUI($"Popups/{popup}");
+        private static PackedScene LoadUI(string ui) => Load($"UI/{ui}");
+        private static PackedScene Load(string prefab) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{prefab}.tscn");
     }
 }
