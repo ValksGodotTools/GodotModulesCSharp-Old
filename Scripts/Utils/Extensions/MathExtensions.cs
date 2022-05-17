d9d3eeabfd651b860e9461f2d434e5a39749559c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-17 17:04:14 -0500

    remove unused math

--- Scripts/Utils/Extensions/MathExtensions.cs
+++ Scripts/Utils/Extensions/MathExtensions.cs
@@ -9,15 +9,5 @@ namespace GodotModules
 
         public static float ToRadians(this float degrees) => 
             degrees * (Mathf.Pi / 180);
-
-        public static float LerpAngle(this float from, float to, float weight) => 
-            from + ShortAngleDistance(from, to) * weight;
-
-        private static float ShortAngleDistance(float from, float to)
-        {
-            var max_angle = Mathf.Pi * 2;
-            var difference = (to - from) % (max_angle);
-            return ((2 * difference) % (max_angle)) - difference;
-        }
     }
 }
