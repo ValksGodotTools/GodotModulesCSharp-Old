b593c7e713e8e8e531ca91217a0f0e52bd45e61f
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-17 15:01:29 -0500

    Player.cs

---
+++ Scripts/Game/Player.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Player : OtherPlayer
+{
+    public override void _PhysicsProcess(float delta)
+    {
+        _sprite.LookAt(GetGlobalMousePosition());
+        HandleMovement(delta);
+    }
+
+    private void HandleMovement(float delta)
+    {
+        var dir = new Vector2();
+
+        if (Input.IsActionPressed("player_move_up"))
+            dir.y -= 1;
+        if (Input.IsActionPressed("player_move_down"))
+            dir.y += 1;
+        if (Input.IsActionPressed("player_move_left"))
+            dir.x -= 1;
+        if (Input.IsActionPressed("player_move_right"))
+            dir.x += 1;
+
+        var Speed = 250f;
+        MoveAndSlide(dir * Speed * delta * 50);
+    }
+}
