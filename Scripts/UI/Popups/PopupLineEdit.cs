7d1a6881b9865fd523b444fe51f02085247fba39
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-18 12:00:54 -0500

    Start of PopupLineEdit.cs

---
+++ Scripts/UI/Popups/PopupLineEdit.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+public class PopupLineEdit : WindowDialog
+{
+    private void _on_LineEdit_text_changed(string newText)
+    {
+
+    }
+
+    private void _on_PopupLineEdit_popup_hide()
+    {
+        
+    }
+
+    private void _on_Ok_pressed()
+    {
+
+    }
+}
