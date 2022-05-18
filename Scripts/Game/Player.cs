24b813ea145654809f4185d4fa5985340880f6c1
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-18 16:06:06 -0500

    animate player

--- Scripts/Game/Player.cs
+++ Scripts/Game/Player.cs
@@ -5,17 +5,20 @@ namespace GodotModules
     public class Player : OtherPlayer
     {
         [Export] public readonly NodePath NodePathCamera;
+        [Export] public readonly NodePath NodePathAnimatedSprite;
         private Camera2D _camera;
+        private AnimatedSprite _animatedSprite;
 
         public override void _Ready()
         {
             _camera = GetNode<Camera2D>(NodePathCamera);
-            _sprite = GetNode<Sprite>(NodePathSprite);
+            _animatedSprite = GetNode<AnimatedSprite>(NodePathAnimatedSprite);
+            //_sprite = GetNode<Sprite>(NodePathSprite);
         }
 
         public override void _PhysicsProcess(float delta)
         {
-            _sprite.LerpRotationToTarget(GetGlobalMousePosition());
+            //_sprite.LerpRotationToTarget(GetGlobalMousePosition());
             HandleMovement(delta);
             HandleShoot();
         }
@@ -42,14 +45,39 @@ namespace GodotModules
         {
             var dir = new Vector2();
 
-            if (Input.IsActionPressed("player_move_up"))
+            var movingUp = Input.IsActionPressed("player_move_up");
+            var movingDown = Input.IsActionPressed("player_move_down");
+            var movingLeft = Input.IsActionPressed("player_move_left");
+            var movingRight = Input.IsActionPressed("player_move_right");
+
+            if (movingUp) 
+            {
                 dir.y -= 1;
-            if (Input.IsActionPressed("player_move_down"))
+                _animatedSprite.Play("walk_up");
+            }
+
+            if (movingDown) 
+            {
                 dir.y += 1;
-            if (Input.IsActionPressed("player_move_left"))
+                _animatedSprite.Play("walk_down");
+            }
+
+            if (movingLeft) 
+            {
                 dir.x -= 1;
-            if (Input.IsActionPressed("player_move_right"))
+                _animatedSprite.Play("walk_left");
+            }
+
+            if (movingRight) 
+            {
                 dir.x += 1;
+                _animatedSprite.Play("walk_right");
+            }
+
+            if (!movingUp && !movingDown && !movingLeft && !movingRight)
+            {
+                _animatedSprite.Play("idle");
+            }
 
             var Speed = 250f;
             MoveAndSlide(dir * Speed * delta * 50);
