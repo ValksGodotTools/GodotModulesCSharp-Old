4d5ab1ea35c46c8144a602ba1a0c70be0bd6f683
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-19 01:01:41 -0500

    coin plays random starting frame on animation

--- Scripts/Game/Coin.cs
+++ Scripts/Game/Coin.cs
@@ -4,6 +4,15 @@ namespace GodotModules
 {
     public class Coin : RigidBody2D
     {
+        [Export] public readonly NodePath NodePathAnimatedSprite;
+
+        public override void _Ready()
+        {
+            var animatedSprite = GetNode<AnimatedSprite>(NodePathAnimatedSprite);
+            animatedSprite.Frame = (int)GD.RandRange(0, animatedSprite.Frames.GetFrameCount("Coin"));
+            animatedSprite.Playing = true;
+        }
+
         public override void _IntegrateForces(Physics2DDirectBodyState state)
         {
             RotationDegrees = 0;
