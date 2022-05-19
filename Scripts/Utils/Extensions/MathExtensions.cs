d46e80457f540e97dc3fb6332ce904d48f466aa0
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-18 19:23:42 -0500

    Add more math extensions

--- Scripts/Utils/Extensions/MathExtensions.cs
+++ Scripts/Utils/Extensions/MathExtensions.cs
@@ -12,5 +12,10 @@ namespace GodotModules
 
         public static float ToRadians(this float degrees) =>
             degrees * (Mathf.Pi / 180);
+
+        public static int Clamp(this int v, int min, int max) => Mathf.Clamp(v, min, max);
+        public static float Clamp(this float v, float min, float max) => Mathf.Clamp(v, min, max);
+        public static float Lerp(this float a, float b, float t) => Mathf.Lerp(a, b, t); 
+        public static Vector2 Lerp(this Vector2 a, Vector2 b, float t) => Utils.Lerp(a, b, t);
     }
 }
