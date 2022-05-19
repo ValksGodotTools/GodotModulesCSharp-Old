116674d9d21a239592a7a7a3985f06473b7e9491
Brian Shao <dev@aercie.com>
2022-05-18 23:02:11 -0700

    Use dictionary for mouse button text display

--- Scripts/Utils/Extensions/HotkeyExtensions.cs
+++ Scripts/Utils/Extensions/HotkeyExtensions.cs
@@ -4,53 +4,25 @@ namespace GodotModules
 {
     public static class HotkeyExtensions 
     {
+        private static readonly Dictionary<int, string> _mouseButtons = new Dictionary<int, string>
+        {
+            { (int)ButtonList.Left, "Left Click" },
+            { (int)ButtonList.Right, "Right Click" },
+            { (int)ButtonList.Middle, "Middle Click" },
+            { (int)ButtonList.Xbutton1, "Mouse XBtn 1" },
+            { (int)ButtonList.Xbutton2, "Mouse XBtn 2" },
+            { (int)ButtonList.WheelUp, "Wheel Up" },
+            { (int)ButtonList.WheelDown, "Wheel Down" },
+            { (int)ButtonList.WheelLeft, "Wheel Left" },
+            { (int)ButtonList.WheelRight, "Wheel Right" },
+        };
+
         public static string Display(this InputEvent e)
         {
-            if (e is InputEventKey k)
-                return k.AsText();
+            if (e is InputEventMouseButton m && _mouseButtons.ContainsKey(m.ButtonIndex))
+                return _mouseButtons[m.ButtonIndex];
             
-            if (e is InputEventMouseButton m)
-                return Display(m);
-            
-            if (e is InputEventJoypadButton j)
-                return j.AsText();
-            
-            throw new InvalidOperationException("Unknown InputEvent type.");
-        }
-
-        private static string Display(InputEventMouseButton m)
-        {
-            switch (m.ButtonIndex)
-            {
-                case (int)ButtonList.Left:
-                    return "Left Click";
-
-                case (int)ButtonList.Right:
-                    return "Right Click";
-
-                case (int)ButtonList.Middle:
-                    return "Middle Click";
-
-                case (int)ButtonList.Xbutton1:
-                    return "Mouse XBtn 1";
-
-                case (int)ButtonList.Xbutton2:
-                    return "Mouse XBtn 2";
-
-                case (int)ButtonList.WheelDown:
-                    return "Wheel Down";
-
-                case (int)ButtonList.WheelUp:
-                    return "Wheel Up";
-
-                case (int)ButtonList.WheelLeft:
-                    return "Wheen Left";
-
-                case (int)ButtonList.WheelRight:
-                    return "Wheel Right";
-            }
-
-            return m.AsText();
+            return e.AsText();
         }
     }
 }
