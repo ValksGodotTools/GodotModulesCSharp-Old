52e207c9fd5c24f7ab32e17964b646179a3373e8
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-20 00:19:35 -0500

    link game server btn signals

--- Scripts/Scenes/SceneGameServers.cs
+++ Scripts/Scenes/SceneGameServers.cs
@@ -8,5 +8,20 @@ namespace GodotModules
         {
             
         }
+
+        private void _on_Create_Lobby_pressed()
+        {
+
+        }
+
+        private void _on_Direct_Connect_pressed()
+        {
+
+        }
+
+        private void _on_Refresh_pressed()
+        {
+            
+        }
     }
 }
