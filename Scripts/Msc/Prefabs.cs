0a83b2b2bf8d7851eb641dc45691cb29e3aa5c46
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-21 16:37:45 -0500

    Reorganize prefabs

--- Scripts/Msc/Prefabs.cs
+++ Scripts/Msc/Prefabs.cs
@@ -16,15 +16,19 @@ namespace GodotModules
         public readonly static PackedScene PopupLineEdit = LoadPopup("PopupLineEdit");
         public readonly static PackedScene PopupCreateLobby = LoadPopup("PopupCreateLobby");
 
-        // Game
-        public readonly static PackedScene Player = LoadGame("Player");
-        public readonly static PackedScene OtherPlayer = LoadGame("OtherPlayer");
-        public readonly static PackedScene Enemy = LoadGame("Enemy");
-        public readonly static PackedScene Orb = LoadGame("Orb");
+        // 2D
+        public readonly static PackedScene Player = Load2D("Player");
+        public readonly static PackedScene OtherPlayer = Load2D("OtherPlayer");
+        public readonly static PackedScene Enemy = Load2D("Enemy");
+        public readonly static PackedScene Orb = Load2D("Orb");
 
-        private static PackedScene LoadGame(string game) => Load($"Game/{game}");
-        private static PackedScene LoadPopup(string popup) => LoadUI($"Popups/{popup}");
-        private static PackedScene LoadUI(string ui) => Load($"UI/{ui}");
-        private static PackedScene Load(string prefab) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{prefab}.tscn");
+        // 3D
+        public readonly static PackedScene Player3D = Load3D("Player");
+
+        private static PackedScene Load2D(string path) => Load($"2D/{path}");
+        private static PackedScene Load3D(string path) => Load($"3D/{path}");
+        private static PackedScene LoadPopup(string path) => LoadUI($"Popups/{path}");
+        private static PackedScene LoadUI(string path) => Load($"UI/{path}");
+        private static PackedScene Load(string path) => ResourceLoader.Load<PackedScene>($"res://Scenes/Prefabs/{path}.tscn");
     }
 }
