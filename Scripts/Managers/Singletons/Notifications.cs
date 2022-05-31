d910e93ecae96d00e782598a48f51ef3d5f40ad1
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-31 18:41:04 -0500

    Notifications.RemoveInvalidListeners()

--- Scripts/Managers/Singletons/Notifications.cs
+++ Scripts/Managers/Singletons/Notifications.cs
@@ -10,11 +10,16 @@ namespace GodotModules
                 _listeners.Add(eventType, new List<Listener>());
 
             foreach (var pair in _listeners)
-                foreach (var listener in pair.Value)
-                    if (sender.GetInstanceId() == listener.Id)
-                        throw new InvalidOperationException($"Tried to add duplicate listener of event type '{eventType}'");
+                foreach (var listener in pair.Value) 
+                {
+                    if (!Godot.Object.IsInstanceValid(listener.Sender))
+                        continue;
 
-            _listeners[eventType].Add(new Listener(sender.GetInstanceId(), action));
+                    if (sender.GetInstanceId() == listener.Sender.GetInstanceId())
+                        throw new InvalidOperationException($"Tried to add duplicate listener of event type '{eventType}'");
+                }
+
+            _listeners[eventType].Add(new Listener(sender, action));
         }
 
         public static void RemoveListener(Node sender, string eventType)
@@ -26,7 +31,7 @@ namespace GodotModules
 
             foreach (var pair in _listeners)
                 for (int i = pair.Value.Count - 1; i >= 0; i--)
-                    if (sender.GetInstanceId() == pair.Value[i].Id) 
+                    if (sender.GetInstanceId() == pair.Value[i].Sender.GetInstanceId()) 
                     {
                         found = true;
                         pair.Value.RemoveAt(i);
@@ -34,6 +39,27 @@ namespace GodotModules
 
             if (!found)
                 throw new InvalidOperationException($"Tried to remove non-existent event type '{eventType}' from listeners");
+        }
+
+        public static void RemoveAllListeners() => _listeners.Clear();
+
+        public static void RemoveInvalidListeners()
+        {
+            var tempListeners = new Dictionary<string, List<Listener>>();
+
+            foreach (var pair in _listeners) 
+            {
+                for (int i = pair.Value.Count - 1; i >= 0; i--)
+                {
+                    if (!Godot.Object.IsInstanceValid(pair.Value[i].Sender))
+                        pair.Value.RemoveAt(i);
+                }
+
+                if (pair.Value.Count > 0)
+                    tempListeners.Add(pair.Key, pair.Value);
+            }
+
+            _listeners = new(tempListeners);
         }
 
         public static void Notify(Node sender, string eventType, params object[] args)
@@ -45,12 +71,12 @@ namespace GodotModules
 
         private class Listener 
         {
-            public ulong Id { get; set; }
+            public Node Sender { get; set; }
             public Action<Node, object[]> Action { get; set; }
 
-            public Listener(ulong id, Action<Node, object[]> action)
+            public Listener(Node sender, Action<Node, object[]> action)
             {
-                Id = id;
+                Sender = sender;
                 Action = action;
             }
         }
