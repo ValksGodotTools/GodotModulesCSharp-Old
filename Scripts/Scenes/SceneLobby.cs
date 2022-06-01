c2e356d8bca28342ccc4a47502c8750b6d962442
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-06-01 00:40:05 -0500

    UILobby -> SceneLobby

--- Scripts/Scenes/SceneLobby.cs
+++ Scripts/Scenes/SceneLobby.cs
@@ -1,6 +1,6 @@
 namespace GodotModules 
 {
-    public class UILobby : Node
+    public class SceneLobby : AScene
     {
         [Export] protected readonly NodePath NodePathLobbyName;
         [Export] protected readonly NodePath NodePathLobbyPlayerCount;
@@ -12,12 +12,27 @@ namespace GodotModules
         private Control _playerList;
         private RichTextLabel _chatLogs;
 
+        private Managers _managers;
+
+        public override void PreInitManagers(Managers managers)
+        {
+            _managers = managers;
+        }
+
         public override void _Ready()
         {
             _lobbyName = GetNode<Label>(NodePathLobbyName);
             _lobbyPlayerCount = GetNode<Label>(NodePathLobbyPlayerCount);
             _playerList = GetNode<Control>(NodePathPlayerList);
             _chatLogs = GetNode<RichTextLabel>(NodePathChatLogs);
+
+            Notifications.AddListener(this, Event.OnKeyPressed, (sender, args) => {
+                _managers.ManagerScene.HandleEscape(async () => {
+                    _managers.Tokens.Cancel("client_running");
+                    _managers.Tokens.Cancel("server_running");
+                    await _managers.ManagerScene.ChangeScene(GameScene.GameServers);
+                });
+            });
         }
 
         private void _on_Chat_Input_text_entered(string text)
